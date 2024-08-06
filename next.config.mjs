f20392d15d6d45515ca407d08a00afb7fb3c0604
unknown <raflisilehu20@gmail.com>
2024-08-07 06:20:05 +0700

    fix(common): fix problems in configuring

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.resolve.alias["@"] = path.resolve("./src");
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
