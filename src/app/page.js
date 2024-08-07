41ce0909a14be990879a5b496e04ca8a9809af39
Josumaru <jokosupriyanto1712@gmail.com>
2024-08-07 20:33:40 +0700

    feat(landing page): create carousel swiper

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import { Navbar } from "@/components/common/layout/navbar";
 import HeaderSection from "@/components/home/header/header";
 import ContactUsSection from "@/components/home/contactUs/contactUs";
+import PosterCarousel from "@/components/home/posterCarousel/posterCarousel";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HeaderSection />
+      <PosterCarousel />
       <ContactUsSection />
     </>
   );
