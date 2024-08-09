e4388aa89be951e9b9b77e04e41e4a16ad9598e0
Josumaru <jokosupriyanto1712@gmail.com>
2024-08-09 13:02:13 +0700

    fix(carousel): change carousel aspect ratio to 16/9 for all devices

--- src/components/home/posterCarousel/posterCarousel.jsx
+++ src/components/home/posterCarousel/posterCarousel.jsx
@@ -4,84 +4,33 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import {
-  Navigation,
-  Pagination,
-  EffectCoverflow,
-  Autoplay,
-  Controller,
-} from "swiper/modules";
 
 import { PosterConstants } from "@/constants/posterConstants";
 import Image from "next/image";
 import { ArrowLeft } from "lucide-react";
 import { ArrowRight } from "lucide-react";
-import { useState, useEffect } from "react"
-import styles from "./styles.module.css";
+import { useState } from "react"
+import { CarouselConstants } from "@/constants/carouselConstants";
 
 const PosterCarousel = ({}) => {
-  const [position, setPosition] = useState(1)
+  const [position, setPosition] = useState(3)
+
+  const handleSlideChange = (e) => {
+    setPosition(e.realIndex);
+  }
   
   return (
-    <Swiper
-    modules={[Pagination, Navigation, Autoplay, EffectCoverflow]}
-    spaceBetween={-40}
-    grabCursor={true}
-    centeredSlides={true}
-    // loop={true}
-    onSlideChange={(e) => setPosition(e.activeIndex)}
-    slidesPerView={"auto"}
-      breakpoints={{
-        640: {
-          slidesPerView: 1,
-          slidesPerGroup: 1,
-        },
-        768: {
-          slidesPerView: 2,
-          slidesPerGroup: 1,
-        },
-        1024: {
-          slidesPerView: 2,
-          slidesPerGroup: 1,
-        },
-      }}
-      effect={"coverflow"}
-      coverflowEffect={{
-        rotate: 0,
-        stretch: 10,
-        depth: 150,
-        modifier: 5,
-      }}
-      autoplay={{
-        delay: 3000,
-        disableOnInteraction: false,
-      }}
-      pagination={{
-        el: ".swiper-pagination",
-        clickable: true,
-        bulletActiveClass: ".swiper-bullet",
-      }}
-      navigation={{
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      }}
-    >
+    <Swiper {...CarouselConstants.swiperOptions} className="!p-3 mt-20" onSlideChange={handleSlideChange}>
       {PosterConstants.map((poster, index) => (
-        <SwiperSlide key={index} className="overflow-hidden">
-            <div className={`flex justify-center items-center py-10`}>
-              <div className={`${styles.image} bg-white`}>
-                <Image
-                    src={poster}
-                    alt={`Poster-${index}`}
-                    className={`${styles.image}`}
-                />
-              </div>
-            </div>
+        <SwiperSlide key={index} className={`rounded-lg`}>
+          <div className="relative lg:w-full rounded-lg overflow-hidden" style={{ paddingBottom: '56.25%' }}>
+            <Image src={poster} alt={`Poster-${index}`} className="absolute top-0 overflow-hidden rounded-2xl left-0 w-full h-full object-cover" />
+          </div>
         </SwiperSlide>
       ))}
-        <div className="!flex items-center justify-center h-5">
+        <div className="!flex items-center justify-center h-5 mt-5">
           <ArrowLeft className="swiper-button-prev !relative mx-4" color="gray"/>
-            {PosterConstants.map((_, index) => <div key={index} className={`rounded-full w-3 h-3 z-20 mx-1`} style={{backgroundColor:  index === position ? "#9700ff" : "#4c4c4c"}}/>)}
+            {PosterConstants.map((_, index) => <div key={index} className={`rounded-full w-3 h-3 z-20 mx-1`} style={{backgroundColor:  index === position ? "#616BDA" : "#4c4c4c"}}/>)}
           <ArrowRight className="swiper-button-next !relative mx-4" color="gray"/>
       </div>
     </Swiper>
--- src/constants/posterConstants.js
+++ src/constants/posterConstants.js
@@ -1,10 +1,10 @@
 import poster1 from "../../public/images/poster/1.png"
-import poster2 from "../../public/images/poster/2.png"
-import poster3 from "../../public/images/poster/3.png"
-import poster4 from "../../public/images/poster/4.png"
-import poster5 from "../../public/images/poster/5.png"
-import poster6 from "../../public/images/poster/5.png"
-import poster7 from "../../public/images/poster/5.png"
+import poster2 from "../../public/images/poster/1.png"
+import poster3 from "../../public/images/poster/1.png"
+import poster4 from "../../public/images/poster/1.png"
+import poster5 from "../../public/images/poster/1.png"
+import poster6 from "../../public/images/poster/1.png"
+import poster7 from "../../public/images/poster/1.png"
 
 export const PosterConstants = [
     poster1,
