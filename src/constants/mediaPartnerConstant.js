0b2c702b4c6f0288d651ac7d76191a3266110b03
unknown <raflisilehu20@gmail.com>
2024-09-05 02:06:28 +0700

    style(home): add media partner

--- src/constants/mediaPartnerConstant.js
+++ src/constants/mediaPartnerConstant.js
@@ -2,10 +2,12 @@ import himatif from "../../public/images/mediaPartner/himatif.png"
 import infoEvent from "../../public/images/mediaPartner/info-event.png"
 import radik from "../../public/images/mediaPartner/radik.png"
 import himakom from "../../public/images/mediaPartner/himakom.png"
+import finic from "../../public/images/mediaPartner/finic.png"
 
 export const MediaPartnerConstant = [
     himatif,
     infoEvent,
     radik,
-    himakom
+    himakom,
+    finic
 ]
