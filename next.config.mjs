cb8c09d6ecb12498befde2744a2eb1b41502b938
unknown <raflisilehu20@gmail.com>
2024-10-04 08:29:03 +0700

    fix(common): fix bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { webpack }) => {
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
-      })
-    );
-    config.devtool = false;
-    return config;
-  },
-  images: {
-    remotePatterns: [
-      {
-        hostname: `${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co`,
-      },
-    ],
-  },
+    webpack: (config, { webpack }) => {
+        config.plugins.push(new webpack.IgnorePlugin({
+            resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
+        }))
+        return config
+    },
+    images: {
+        remotePatterns: [
+            {
+                hostname: `${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co`
+            }
+        ]
+    },
 };
 
 export default nextConfig;
